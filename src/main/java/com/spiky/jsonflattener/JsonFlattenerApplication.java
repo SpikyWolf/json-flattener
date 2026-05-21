54d7a99585d39cb918589ff633e9c77274e1e33d
EternalFlame <philippe.callebaut@gmail.com>
2026-05-21 03:57:03 +0200

    Final commit to push v0.1.0
    Signed-off-by: EternalFlame <philippe.callebaut@gmail.com>

--- src/main/java/com/spiky/jsonflattener/JsonFlattenerApplication.java
+++ src/main/java/com/spiky/jsonflattener/JsonFlattenerApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.jetbrains.annotations.NotNull;
 
@@ -23,6 +24,7 @@ public class JsonFlattenerApplication extends Application {
         new MainViewController(root, model);
 
         stage.setTitle("JSON Flattener");
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/spiky/jsonflattener/icon.png"))));
         stage.setMinWidth(640);
         stage.setMinHeight(360);
         stage.setScene(scene);
